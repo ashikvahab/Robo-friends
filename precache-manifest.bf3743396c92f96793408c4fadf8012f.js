0cb01f84cb36a4f9251189937c5048fc248b9fd4
ashikvahab <ashik.w@gmail.com>
2020-03-01 15:33:43 -0500

    Updates

---
+++ precache-manifest.bf3743396c92f96793408c4fadf8012f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fead0b1867fefbfdbab90d11c621ec6",
+    "url": "/ashikvahab/Robo-friends.git/index.html"
+  },
+  {
+    "revision": "7252af28cea19875d81c",
+    "url": "/ashikvahab/Robo-friends.git/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "dffaa0bc372a92ddc895",
+    "url": "/ashikvahab/Robo-friends.git/static/css/main.5f650857.chunk.css"
+  },
+  {
+    "revision": "7252af28cea19875d81c",
+    "url": "/ashikvahab/Robo-friends.git/static/js/2.2333c5ef.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ashikvahab/Robo-friends.git/static/js/2.2333c5ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dffaa0bc372a92ddc895",
+    "url": "/ashikvahab/Robo-friends.git/static/js/main.08725e9c.chunk.js"
+  },
+  {
+    "revision": "8ff199c4864a6e0576e5",
+    "url": "/ashikvahab/Robo-friends.git/static/js/runtime-main.d621705c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/ashikvahab/Robo-friends.git/static/media/SEGA.12a3db8b.woff"
+  }
+]);
